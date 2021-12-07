72179fda7de0b227184c8cd0689f8963c4b2db70
fer-dev-mx <62954322+fer-dev-mx@users.noreply.github.com>
2021-12-07 08:58:50 -0600

    Add Location and Number of Seats properties
    Create Showing class
    
    Add Location property
    
    Add Number of Seats property

---
+++ RockyHorror/RockyHorror/Showing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockyHorror
+{
+    public class Showing
+    {
+        public string Location { get; set; }
+        public int NumberOfSeats { get; set; }
+    }
+}
